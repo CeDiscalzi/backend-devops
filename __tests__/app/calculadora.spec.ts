1afcb21b9fea690f39447416f9afa16392904c36
Carlos Discalzi <carlos.discalzi@gmail.com>
2024-11-25 19:15:59 -0300

    Configuracion de pruebas con jest

--- __tests__/app/calculadora.spec.ts
+++ __tests__/app/calculadora.spec.ts
@@ -1,5 +1,6 @@
 import { describe, test, expect } from "@jest/globals";
-import { suma, restar, operar } from "../../src/app/calculadora";
+import { suma, restar, operar, multiplicar, dividir, exponencial, factorial } from "../../src/app/calculadora";
+import { randomInt } from "crypto";
 
 describe("Bateria de test de calculadora", () => {
 
@@ -10,15 +11,12 @@ describe("Bateria de test de calculadora", () => {
     test("Sumar en la calculadora", () => {
 
         expect(suma(1, 2)).toBe(3);
-
         expect(suma(5, 5)).toBe(10);
-
         expect(suma(15, 15)).toBe(30);
-
         expect(suma(1, 5)).not.toBe(0);
 
         let a: any = 1;
-        let b: any = "a"
+        let b: any = "a";
         expect(suma(a, b)).toBeNaN();
         a = 1;
         b = undefined;
@@ -27,20 +25,85 @@ describe("Bateria de test de calculadora", () => {
     });
 
     test("Restar en la calculadora", () => {
+
         expect(restar(5, 2)).toBe(3);
-
         expect(restar(15, 5)).toBe(10);
-
         expect(restar(45, 15)).toBe(30);
-
         expect(restar(10, 5)).not.toBe(0);
 
         let a: any = 1;
-        let b: any = "a"
+        let b: any = "a";
         expect(restar(a, b)).toBeNaN();
         a = 1;
         b = undefined;
         expect(() => restar(a, b)).toThrowError("No se puede restar indefinidos");
+    });
+
+    test("Multiplicar en la calculadora", () => {
+
+        expect(multiplicar(5, 2)).toBe(10);
+        expect(multiplicar(-5, 2)).toBe(-10);
+        expect(multiplicar(15, 5)).toBe(75);
+        expect(multiplicar(45, 15)).toBe(675);
+        expect(multiplicar(10, 0)).toBe(0);
+        expect(multiplicar(2, 2)).not.toBe(3);
+
+        let a: any = 1;
+        let b: any = "a";
+        expect(multiplicar(a, b)).toBeNaN();
+        a = 1;
+        b = undefined;
+        expect(() => multiplicar(a, b)).toThrowError("No se puede multiplicar indefinidos");
+    });
+
+    test("Dividir en la calculadora", () => {
+
+        expect(dividir(10, 2)).toBe(5);
+        expect(dividir(15, 5)).toBe(3);
+        expect(dividir(45, 15)).toBe(3);
+        expect(() => dividir(10, 0)).toThrowError("No se puede dividir por cero");
+
+        let a: any = 0;
+        let b: any = randomInt(1, 100);
+        expect(dividir(0, b)).toBe(0);
+
+        a = 1;
+        b = "a";
+        expect(dividir(a, b)).toBeNaN();
+        a = 1;
+        b = undefined;
+        expect(() => dividir(a, b)).toThrowError("No se puede dividir indefinidos");
+    });
+
+    test("Exponencial en la calculadora", () => {
+
+        expect(exponencial(-10, -2)).toBe(0.01);
+        expect(exponencial(15, 5)).toBe(759375);
+        expect(exponencial(8, 5)).toBe(32768);
+
+        let a: any = 1;
+        let b: any = "a";
+        expect(exponencial(a, b)).toBeNaN();
+
+        a = 1;
+        b = undefined;
+        expect(() => exponencial(a, b)).toThrowError("No se puede utilizar esta funcion con valores indefinidos");
+    });
+
+    test("Factorial en la calculadora", () => {
+
+        expect(factorial(2)).toBe(2);
+        expect(factorial(8)).toBe(40320);
+        expect(factorial(0)).toBe(1);
+
+        let a: any = "a"
+        expect(factorial(a)).toBeNaN();
+
+        a = undefined;
+        expect(() => factorial(a)).toThrowError("No se puede utilizar esta funcion con valores indefinidos");
+
+        a = -5;
+        expect(() => factorial(a)).toThrowError("El factorial no está definido para números negativos");
     });
 
     test("Operar en la calculadora", () => {
@@ -53,9 +116,33 @@ describe("Bateria de test de calculadora", () => {
         expect(operar("resta", 45, 15)).toBe(30);
         expect(operar("suma", 1, 5)).not.toBe(0);
 
+        expect(operar("multiplicar", 5, 2)).toBe(10);
+        expect(operar("multiplicar", -5, 2)).toBe(-10);
+        expect(operar("multiplicar", 15, 5)).toBe(75);
+        expect(operar("multiplicar", 45, 15)).toBe(675);
+        expect(operar("multiplicar", 10, 0)).toBe(0);
+        expect(operar("multiplicar", 2, 2)).not.toBe(3);
+
+        expect(operar("dividir", 10, 2)).toBe(5);
+        expect(operar("dividir", -10, 2)).toBe(-5);
+        expect(operar("dividir", 15, 5)).toBe(3);
+        expect(operar("dividir", 45, 15)).toBe(3);
+
+        expect(operar("exponencial", -10, -2)).toBe(0.01);
+        expect(operar("exponencial", 15, 5)).toBe(759375);
+        expect(operar("exponencial", 8, 5)).toBe(32768);
+
+        expect(operar("factorial", 2)).toBe(2);
+        expect(operar("factorial", 8)).toBe(40320);
+        expect(operar("factorial", 0)).toBe(1);
+
+        expect(() => operar("factorial", -25)).toThrowError("El factorial no está definido para números negativos");
+
+
         let a: any = 1;
-        let b: any = "a"
+        let b: any = "a";
         expect(operar("suma", a, b)).toBeNaN();
+
         a = 1;
         b = undefined;
         expect(() => operar("suma", a, b)).toThrowError("No se puede sumar indefinidos");
@@ -64,9 +151,22 @@ describe("Bateria de test de calculadora", () => {
         b = undefined;
         expect(() => operar("resta", a, b)).toThrowError("No se puede restar indefinidos");
 
+
+        a = 1;
+        b = undefined;
+        expect(() => operar("multiplicar", a, b)).toThrowError("No se puede multiplicar indefinidos");
+
+        a = 1;
+        b = undefined;
+        expect(() => operar("dividir", a, b)).toThrowError("No se puede dividir indefinidos");
+
+
+        a = 2;
+        b = 0;
+        expect(() => operar("dividir", a, b)).toThrowError("No se puede dividir por cero");
+
         expect(operar(undefined, 10, 20)).not.toBeDefined();
 
-        expect(operar("multiplicar", 10, 20)).not.toBeDefined();
     });
 
 });
--- src/app/calculadora.ts
+++ src/app/calculadora.ts
@@ -1,8 +1,16 @@
-function operar(operacion: string = "", a: number, b: number) {
+function operar(operacion: string = "", a: number, b: any = undefined) {
     if (operacion === 'suma') {
         return suma(a, b);
     } else if (operacion === 'resta') {
         return restar(a, b);
+    } else if (operacion === 'multiplicar') {
+        return multiplicar(a, b);
+    } else if (operacion === 'dividir') {
+        return dividir(a, b);
+    } else if (operacion === 'exponencial') {
+        return exponencial(a, b);
+    } else if (operacion === 'factorial') {
+        return factorial(a);
     }
 }
 
@@ -30,4 +38,64 @@ function restar(a: number, b: number) {
     return a - b;
 }
 
-export { suma, operar, restar };
+function multiplicar(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+
+        throw new Error("No se puede multiplicar indefinidos");
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+    return a * b;
+}
+
+function dividir(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+        throw new Error("No se puede dividir indefinidos");
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+    if (b === 0) {
+        throw new Error("No se puede dividir por cero");
+    }
+    if (a === 0) {
+        return 0
+    }
+
+    return a / b;
+}
+
+function exponencial(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+
+        throw new Error("No se puede utilizar esta funcion con valores indefinidos");
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+    return (a) ** b;
+}
+
+function factorial(a: number) {
+    if (a === undefined) {
+
+        throw new Error("No se puede utilizar esta funcion con valores indefinidos");
+    }
+    if (typeof a !== 'number') {
+        return NaN;
+    }
+    if (a === 0 || a === 1) {
+        return 1;
+    }
+    if (a < 0) {
+        throw new Error("El factorial no está definido para números negativos");
+    }
+
+    for (var i = a - 1; i >= 1; i--) {
+        a *= i;
+    }
+    return a;
+}
+
+export { suma, operar, restar, multiplicar, dividir, exponencial, factorial };
